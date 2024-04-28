99e94780387d75c5554af79ed43fe03bf219eaf3
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-04-28 19:17:03 +0300

    Implement login

--- project/vulnerableApp/urls.py
+++ project/vulnerableApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import loginView
+from .views import mainView, loginView
 
 urlpatterns = [
-    path('', loginView, name='login'),
+    path('', mainView, name='main'),
+    path('login', loginView, name='login'),
 ]
--- project/vulnerableApp/views.py
+++ project/vulnerableApp/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def loginView(request):
   if request.method == 'POST':
-    print(request.POST)
+    user = authenticate(
+      request=request,
+      username=request.POST['username'],
+      password=request.POST['password']
+    )
 
-  return render(request, 'login.html')
+    if user is not None:
+      login(request, user)
+
+  return redirect('/')
+
+def mainView(request):
+  if not request.user.is_authenticated:
+    return render(request, 'login.html')
+
+  return render(request, 'main.html')
