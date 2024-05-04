c951828c258dc9c5ad4aa1db1b22c665dce7b9ce
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-05-04 14:33:12 +0300

    Implement SQL injection

--- project/vulnerableApp/urls.py
+++ project/vulnerableApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import mainView, loginView, addView
+from .views import mainView, loginView, addView, usermessagesView
 
 urlpatterns = [
     path('', mainView, name='main'),
     path('login', loginView, name='login'),
     path('send', addView, name='send'),
+    path('usermessages', usermessagesView, name='usermessages'),
 ]
--- project/vulnerableApp/views.py
+++ project/vulnerableApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from django.db import connection
 
 from .models import Message
 
@@ -34,3 +35,13 @@ def mainView(request):
   messages = Message.objects.all()
 
   return render(request, 'main.html', {'messages': messages})
+
+def usermessagesView(request):
+  if request.method == 'POST':
+    with connection.cursor() as cursor:
+      cursor.execute(f"SELECT content, time FROM vulnerableApp_message WHERE sender_id='{request.POST['user']}'")
+
+      results = cursor.fetchall()
+      messages = [{"content": result[0], "time": result[1]} for result in results]
+
+  return render(request, 'messages.html', {'messages': messages})
